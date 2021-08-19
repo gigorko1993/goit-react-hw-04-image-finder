84d28b7128c663408c9af4b16d448fd07ef1951f
gigorko1993 <gigorko1993@gmail.com>
2021-08-20 00:57:32 +0300

    made refactoring on react hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer, toast } from 'react-toastify';
@@ -13,123 +13,107 @@ import s from './App.module.css';
 
 import { fetchImages } from './Components/api';
 
-class App extends Component {
-  state = {
-    value: '',
-    page: 1,
-    images: [],
-    status: 'idle',
-    loader: false,
-    showModal: false,
-    modalContent: null,
-  };
+const STATUS = {
+  IDLE: 'idle',
+  RESOLVED: 'resolved',
+  REJECT: 'reject',
+};
+export default function App() {
+  const [value, setValue] = useState('');
+  const [page, setPage] = useState(1);
+  const [images, setImages] = useState([]);
+  const [status, setStatus] = useState(STATUS.IDLE);
+  const [loader, setLoader] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [modalContent, setModalContent] = useState(null);
 
-  componentDidUpdate(prevProps, prevState) {
-    const { page, value } = this.state;
-    if (value !== prevState.value || page !== prevState.page) {
-      if (page === 1) {
-        this.resetImages();
-      }
-
-      this.setState({ loader: true });
-
-      fetchImages(page, value)
-        .then(res => {
-          if (res.hits.length < 12) {
-            toast.warn('All images are loaded', {
-              toastId: 'anotherCustomId',
-            });
-          }
-
-          this.setState(prevState => ({
-            images: [...prevState.images, ...res.hits],
-            status: 'resolved',
-          }));
-        })
-        .catch(() => this.setState({ status: 'rejected' }))
-        .finally(() => {
-          this.setState({ loader: false });
-          window.scrollTo({
-            top: document.documentElement.scrollHeight,
-            behavior: 'smooth',
-          });
-        });
+  useEffect(() => {
+    if (value === '') {
+      return;
     }
-  }
+    if (page === 1) {
+      setImages([]);
+    }
+    setLoader(true);
 
-  setValue = val => {
-    this.setState(prevState => {
-      if (prevState.value === val.toLowerCase()) {
-        alert('alert');
-        toast.dark('Try to change your input :)', {
-          toastId: 'customId',
+    fetchImages(page, value)
+      .then(res => {
+        if (res.hits.length < 12) {
+          toast.warn('All images are loaded', {
+            toastId: 'anotherCustomId',
+          });
+        }
+
+        setImages(p => [...p, ...res.hits]);
+        setStatus(STATUS.RESOLVED);
+      })
+      .catch(() => setStatus(STATUS.REJECT))
+      .finally(() => {
+        setLoader(false);
+        window.scrollTo({
+          top: document.documentElement.scrollHeight,
+          behavior: 'smooth',
         });
-        return;
-      }
+      });
+  }, [page, value]);
 
-      return { value: val.toLowerCase(), page: 1 };
-    });
+  const setQuery = val => {
+    if (value === val.toLowerCase()) {
+      alert('alert');
+      toast.dark('Try to change your input :)', {
+        toastId: 'customId',
+      });
+      return;
+    }
+    setValue(val.toLowerCase());
+    setPage(1);
   };
 
-  resetImages = () => {
-    this.setState({ images: [] });
+  const onImageClick = id => {
+    const modalImg = images.find(el => el.id === id);
+    setModalContent(modalImg);
+    setShowModal(p => !p);
   };
 
-  setPage = () => {
-    this.setState(prevState => {
-      return { page: prevState.page + 1 };
-    });
-  };
+  return (
+    <div>
+      <Searchbar onClick={setQuery} />
 
-  showModal = () => {
-    this.setState(prevState => {
-      return { showModal: !prevState.showModal };
-    });
-  };
-
-  onImageClick = id => {
-    const modalImg = this.state.images.find(el => el.id === id);
-    this.setState({ modalContent: modalImg });
-    this.showModal();
-  };
-
-  render() {
-    const { images, loader, status, showModal, modalContent } = this.state;
-
-    return (
-      <div>
-        <Searchbar onClick={this.setValue} />
-
-        {status === 'idle' ? (
-          <h2 className={s.title}>Input your query to find images</h2>
-        ) : null}
-        {status === 'resolved' ? (
-          <>
-            <ImageGallery images={images} onImageClick={this.onImageClick} />
-            <Button onClick={this.setPage} />
-          </>
-        ) : null}
-        {loader && (
-          <Loader
-            className="Loader"
-            type="Oval"
-            color="#9900cc"
-            height={180}
-            width={180}
+      {status === 'idle' ? (
+        <h2 className={s.title}>Input your query to find images</h2>
+      ) : null}
+      {status === 'resolved' ? (
+        <>
+          <ImageGallery images={images} onImageClick={onImageClick} />
+          <Button
+            onClick={() => {
+              setPage(p => p + 1);
+            }}
           />
-        )}
-        {status === 'rejected' ? (
-          <h2 className={s.title}>Error. Try to change your query</h2>
-        ) : null}
-        {showModal && (
-          <Modal showModal={this.showModal}>
-            <img src={modalContent.largeImageURL} alt={modalContent.tags} />
-          </Modal>
-        )}
-        <ToastContainer />
-      </div>
-    );
-  }
+        </>
+      ) : null}
+      {loader && (
+        <Loader
+          className="Loader"
+          type="Oval"
+          color="#9900cc"
+          height={180}
+          width={180}
+        />
+      )}
+      {status === 'reject' ? (
+        <h2 className={s.title}>Error. Try to change your query</h2>
+      ) : null}
+      {showModal && (
+        <Modal
+          showModal={() => {
+            setShowModal(p => !p);
+          }}
+        >
+          <img src={modalContent.largeImageURL} alt={modalContent.tags} />
+        </Modal>
+      )}
+      <ToastContainer />
+    </div>
+  );
 }
-
-export default App;
